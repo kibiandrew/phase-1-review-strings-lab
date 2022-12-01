34684cadbba717aa724cfe798e40551637025e58
Andrew Kibiwott <kibiandrew@gmail.com>
2022-12-01 07:58:53 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+//const welcomeMessage = 'Welcome to Flatbook, ';
+
+//const welcomeMessage = currentUser;
+//const welcomeMessage = 'Welcome to Flatbook, currentUser';
+const welcomeMessage = `Welcome to Flatbook, + ${currentUser} +!`;
+
+
+// const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+
+//const shortGreeting = 'Welcome,';
+
+const shortGreeting = `Welcome,  + Grace + !`;
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + "GRACE HOPPER" + "!";
